f91bf92a07b5511d784ca436d9a8f0cd5d4266df
Packman700 <53169727+Packman700@users.noreply.github.com>
2021-05-19 12:00:08 +0200

    Add color light correction

--- main.py
+++ main.py
@@ -8,35 +8,38 @@ def convert_to_2d_array(input_list, row_size):
         output_list.append(list(input_list[start_index: start_index + row_size]))
     return output_list
 
-ROW_SIZE = 10
+
+def calculate_correction(photo_width, row_size):
+    MAX_CORRECTION_VALUE = 2.8
+    correction_rate = (photo_width / row_size) % 1
+    return correction_rate * MAX_CORRECTION_VALUE
+
+ROW_SIZE = 184
 TEXT_STRING = "#"
-image = Image.open("Sprite-0001.png")
-# image = Image.open("landscape.png")
+image = Image.open("img_1.png")
 
 img_width = image.width
 img_height = image.height
 
 COLUMN_SIZE = round((img_height * ROW_SIZE) / img_width)
-char_pixel_size = img_height / ROW_SIZE
+char_pixel_size = img_width / ROW_SIZE
 
 img_rgb_2d_list = convert_to_2d_array(list(image.getdata()), img_width)
 
-# print(COLUMN_SIZE)
-# print(ROW_SIZE)
-
 average_rgb_values_2d_list = []
-CHAR_PIXEL_FIELD = char_pixel_size**2
-for x in range(ROW_SIZE):
+
+COLOR_CORRECTION = calculate_correction(img_width, ROW_SIZE)
+
+CHAR_PIXEL_FIELD = char_pixel_size**2 - COLOR_CORRECTION
+for x in range(COLUMN_SIZE):
     x_start_index = math.floor(x * char_pixel_size)
-    rows = img_rgb_2d_list[x_start_index : math.floor(x_start_index + char_pixel_size)]
-    for y in range(COLUMN_SIZE):
-        print(rows)
+    rows = img_rgb_2d_list[x_start_index: math.floor(x_start_index + char_pixel_size)]
+    for y in range(ROW_SIZE):
         y_start_index = math.floor(y * char_pixel_size)
 
         char_color = [0, 0, 0]
         for row in rows:
             row_values = row[y_start_index: math.floor(y_start_index + char_pixel_size)]
-            # print(row_values)
             divided_rgb_list = map(lambda rgb: (rgb[0]/CHAR_PIXEL_FIELD, rgb[1]/CHAR_PIXEL_FIELD, rgb[2]/CHAR_PIXEL_FIELD),
                                    row_values)
             sum_rgb = [sum(rgb) for rgb in zip(*divided_rgb_list)]
@@ -45,7 +48,7 @@ for x in range(ROW_SIZE):
         char_color = (round(char_color[0]), round(char_color[1]), round(char_color[2]))
         average_rgb_values_2d_list.append(char_color)
 
-new_image = Image.new(mode = "RGB", size = (ROW_SIZE, COLUMN_SIZE))
+new_image = Image.new(mode="RGB", size=(ROW_SIZE, COLUMN_SIZE))
 new_image.putdata(average_rgb_values_2d_list)
 new_image.save('test.png')
 # print(img_rgb_2d_list)
