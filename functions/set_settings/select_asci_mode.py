2f8fde4d8c40c758d30a22faf3318880c72fd1cd
Packman700 <53169727+Packman700@users.noreply.github.com>
2021-05-31 17:02:01 +0200

    [Feature] Add new asci modes

--- functions/set_settings/select_asci_mode.py
+++ functions/set_settings/select_asci_mode.py
@@ -1,7 +1,9 @@
 MENU = """  SELECT ASCI MODE
 ---------------------
 1. 10 grey level
-2. 70 grey level
+1. Reverse 10 grey level
+3. 70 grey level
+4. Reverse 70 grey level
 ---------------------"""
 
 def select_asci_mode():
@@ -12,9 +14,13 @@ def select_asci_mode():
 
         MODE = ''
         if CHOSE == '1' or CHOSE == "10 grey level" or CHOSE == "":
-            MODE = "10_grey_level"  # mode 2
-        elif CHOSE == '2' or CHOSE == "70 grey level":
-            MODE = "70_grey_level"  # mode 1
+            MODE = "10_grey_level"
+        elif CHOSE == '2' or CHOSE == "reverse 10 grey level":
+            MODE = "reverse_10_grey_level"
+        elif CHOSE == '3' or CHOSE == "70 grey level":
+            MODE = "70_grey_level"
+        elif CHOSE == '4' or CHOSE == "reverse 70 grey level":
+            MODE = "reverse_70_grey_level"
         else:
             print("Wrong option try again")
             continue
